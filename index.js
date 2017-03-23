05914a02956e6a559ea64ceade62ddc0b0bce44a
mateucamps <mcampshoms@gmail.com>
2017-03-23 13:25:05 +0100

    Caspar.js is now an object

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 var Web = require('./lib/web');
-var config = require('./config.json');
-
-var casparPlato = require('./lib/caspar1');
-var casparDSK = require('./lib/caspar2');
-
-casparDSK.ccgInit(config.ips.ipCasparDSK);
-casparPlato.ccgInit(config.ips.ipCasparPlato);
-
-exports.casparPlato = casparPlato;
-exports.casparDSK = casparDSK;
-
-console.log("IP DSK:", casparDSK.quinaIp());
-console.log("IP PLATO:", casparPlato.quinaIp());
